94a3483d6ffd7d0e391e7d5292876b48f5268ec5
david <dstainton415@gmail.com>
2009-01-31 15:58:17 -0800

    i forgot to write my name ;-)

--- modules.d/eth_bps.py
+++ modules.d/eth_bps.py
@@ -12,6 +12,12 @@
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
 # License for the specific language governing permissions and limitations under
 # the License.
+#
+# author: David Stainton
+#
+
+__author__ = "David Stainton"
+
 
 import re
 
